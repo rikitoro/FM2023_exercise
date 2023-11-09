d36edd05a9b5c1b3d85ff0d38ede81f23acbdfa0
rikitoro <yoshiaki@sendai-nct.ac.jp>
2023-11-09 21:18:08 +0900

    add lattices/sheets2

--- FM2023Exrcise/section05_sets/sheet5.lean
+++ FM2023Exrcise/section05_sets/sheet5.lean
@@ -12,3 +12,39 @@ example : A ∪ A = A :=
   ext w
   rw [mem_union]
   exact or_self_iff
+
+example : A ∩ A = A :=
+  by
+  ext w
+  rw [mem_inter_iff]
+  exact and_self_iff
+
+example : A ∩ ∅ = ∅ :=
+  by
+  ext w
+  rw [mem_inter_iff]
+  apply Iff.intro
+  . intro h
+    rcases h with ⟨_, h2⟩
+    apply h2
+  . intro h
+    apply False.elim
+    apply not_mem_empty x $ h
+
+example : A ∪ univ = univ :=
+  by
+  ext w
+  rw [mem_union]
+  constructor
+  . intro _
+    triv
+  . intro _
+    right
+    triv
+
+example : A ⊆ B → B ⊆ A → A = B :=
+  by
+  intro hAB hBA
+  ext w
+  sorry
+  done
---
+++ FM2023Exrcise/section06_orderings_and_lattices/sheet2.lean
@@ -0,0 +1,61 @@
+import Mathlib.Tactic
+
+variable (L : Type) [Lattice L] (a b c : L)
+
+example : a ⊔ b = b ⊔ a :=
+  by
+  apply le_antisymm
+  repeat
+    . apply sup_le
+      . apply le_sup_right
+      . apply le_sup_left
+  done
+
+example : (a ⊔ b) ⊔ c = a ⊔ (b ⊔ c) :=
+  by
+  apply le_antisymm
+  . apply sup_le
+    . apply sup_le
+      . apply le_sup_left
+      . have h : b ≤ b ⊔ c :=
+          by
+          apply le_sup_left
+        apply le_trans h
+        apply le_sup_right
+    . have h : c ≤ b ⊔ c :=
+        by
+        apply le_sup_right
+      apply le_trans h
+      apply le_sup_right
+  . apply sup_le
+    . have h : a ≤ a ⊔ b :=
+        by
+        apply le_sup_left
+      apply le_trans h
+      apply le_sup_left
+    . apply sup_le
+      . have h : b ≤ a ⊔ b :=
+          by
+          apply le_sup_right
+        apply le_trans h
+        apply le_sup_left
+      . apply le_sup_right
+  done
+
+example (h : b ≤ c) : a ⊓ b ≤ a ⊓ c :=
+  by
+  apply le_inf
+  . apply inf_le_left
+  . have h' : a ⊓ b ≤ b :=
+      by
+      apply inf_le_right
+    apply le_trans h' h
+  done
+
+example : (a ⊓ b) ⊔ (a ⊓ c) ≤ a ⊓ (b ⊔ c) :=
+  by
+  sorry
+
+example : a ⊔ (b ⊓ c) ≤ (a ⊔ b) ⊓ (a ⊔ c) :=
+  by
+  sorry
